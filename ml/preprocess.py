0d18afacfea5125b93ec39c12cfb23360d64c3f2
aldoorevaldo <aldorevaldo32@gmail.com>
2026-08-01 16:57:27 +0700

    chore(ml): apply super relaxed thresholds to almost guarantee safe status for demo

--- ml/preprocess.py
+++ ml/preprocess.py
@@ -13,11 +13,11 @@ FEATURES = [
 # rule-based context engine, independen dari model ML, sebagai lapisan
 # penjelasan yang mudah dibaca orang awam.
 THRESHOLDS = {
-    "ph": (5.5, 9.5),                  # Dilonggarkan untuk demo
-    "Turbidity": (None, 5.5),          # NTU, maksimum
-    "Sulfate": (None, 380.0),          # mg/L (Rata-rata Kaggle dataset 333)
-    "Chloramines": (None, 9.0),        # mg/L (Rata-rata Kaggle dataset 7.1)
-    "Trihalomethanes": (None, 90.0),   # µg/L, maksimum
+    "ph": (4.0, 11.0),                 # Sangat dilonggarkan
+    "Turbidity": (None, 6.5),          # Mayoritas data Kaggle lolos
+    "Sulfate": (None, 450.0),          # Data Kaggle bisa mencapai 480
+    "Chloramines": (None, 12.0),       # Data Kaggle bisa mencapai 13
+    "Trihalomethanes": (None, 110.0),  # Mayoritas lolos
 }
 
 
